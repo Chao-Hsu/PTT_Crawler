cb654964244c3cb29ea34c985d206cff647445e1
Chao-Hsu <66721339+Chao-Hsu@users.noreply.github.com>
2022-09-17 00:50:25 +0800

    move .json to folder

--- crawler.py
+++ crawler.py
@@ -262,11 +262,11 @@ def CheckData(old_data, new_data):
 
 
 def WtiteJson(my_data, filename):
-    json_data = open(f"./{filename}.json", 'w', encoding='utf-8')
+    json_data = open(f"./json/{filename}.json", 'w', encoding='utf-8')
     json.dump(my_data, json_data, ensure_ascii=False)
     json_data.close()
 
 
 def ReadJson(filename):
-    with open(f'{filename}.json', 'r', encoding='utf-8') as f:
+    with open(f'./json/{filename}.json', 'r', encoding='utf-8') as f:
         return json.load(f)
