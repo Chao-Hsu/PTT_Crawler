8c322bb3150af39d58846837a3913753f2020ef9
Chao-Hsu <66721339+Chao-Hsu@users.noreply.github.com>
2022-09-18 01:02:24 +0800

    sleep during midnight

--- test.py
+++ test.py
@@ -55,35 +55,36 @@ def SendTitleMessage(my_data, new_data_id_list):
 
 
 def main(argv):
-    if (len(argv) > 1 and argv[1] == '--delay'):
-        crawler.DoSomeDelay()
+    if datetime.datetime.now().hour >= 8 or datetime.datetime.now().hour <= 4:
+        if (len(argv) > 1 and argv[1] == '--delay'):
+            crawler.DoSomeDelay()
 
-    # title
-    my_title_data = crawler.ReadJson("data_title")
-    crawl_title_data, new_data_id_list = crawler.GetTitleData(
-        url_title, my_title_data)
-    checked_title_data = crawler.CheckData(my_title_data, crawl_title_data)
+        # title
+        my_title_data = crawler.ReadJson("data_title")
+        crawl_title_data, new_data_id_list = crawler.GetTitleData(
+            url_title, my_title_data)
+        checked_title_data = crawler.CheckData(my_title_data, crawl_title_data)
 
-    SendTitleMessage(checked_title_data, new_data_id_list)
+        SendTitleMessage(checked_title_data, new_data_id_list)
 
-    crawler.WtiteJson(checked_title_data, "data_title")
+        crawler.WtiteJson(checked_title_data, "data_title")
 
-    # push
-    crawl_push_data = crawler.GetPushData(url_push)
-    normalized_push_data = crawler.Normalize(crawl_push_data)
-    my_push_data = crawler.ReadJson("data_push")
+        # push
+        crawl_push_data = crawler.GetPushData(url_push)
+        normalized_push_data = crawler.Normalize(crawl_push_data)
+        my_push_data = crawler.ReadJson("data_push")
 
-    SendItemMessage(my_push_data, normalized_push_data)
+        SendItemMessage(my_push_data, normalized_push_data)
 
-    crawler.WtiteJson(normalized_push_data, "data_push")
+        crawler.WtiteJson(normalized_push_data, "data_push")
 
-    # TODO:檢查ID變小
-    # if id變小:
-    #     query=f''
-    #     os.system(query)
-    #     crawler.WtiteJson(my_data,"年+上個月")
-    # else:
-    #     crawler.WtiteJson(crawl_data,"data.json")
+        # TODO:檢查ID變小
+        # if id變小:
+        #     query=f''
+        #     os.system(query)
+        #     crawler.WtiteJson(my_data,"年+上個月")
+        # else:
+        #     crawler.WtiteJson(crawl_data,"data.json")
 
 
 if __name__ == '__main__':
